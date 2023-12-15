b50ab0ac7be1316241aaea738cc931641223e1b5
Alberto Andres Gomez <albertoandresmotogp2@gmail.com>
2023-12-16 00:20:15 +0100

    commit

--- BSTree.h
+++ BSTree.h
@@ -12,56 +12,133 @@ class BSTree{
 		BSNode<T> *root; //Nodo raiz del árbol
 		
 		BSNode<T>* search(BSNode<T> *n, T e) const{
-			if(n == nullptr)//árbol vacío
-				throw runtime_error("Arbol vacío\n");
-			else if(n->elem < e)
-				return search(n->right, e);
+			BSNode<T> *result;
+			if(n == nullptr)
+				throw runtime_error("Value not found in tree\n");
+			else if(n->elem == e)
+				result = n;
 			else if(n->elem > e)
-				return search(n->left, e);
-			else
-				return n;
+				result = search(n->left, e);
+			else if(n->elem < e)
+				result = search(n->right, e);
+			
+			return result;
 		}
 		
 		BSNode<T>* insert(BSNode<T> *n, T e){
 			if(n == nullptr)
-				return new BSNode<T>(e, nullptr, nullptr); //DUDA
-			else if(n->elem == e)
-				throw runtime_error("Elemento duplicado\n");
+				return new BSNode<T>(e);
+			
+			if(n->elem == e)
+				throw runtime_error("Duplicated value\n");
+			else if(n->elem > e)
+				n->left = insert(n->left, e);
 			else if(n->elem < e)
 				n->right = insert(n->right, e);
-			else 
-				n->left = insert(n->left, e);
 			
 			return n;
 		}
 		
-		void print_inorden(ostream &out, BSNode<T> *n) const{
-			if(n != nullptr){
-				print_inorden(out, n->left);
-				out << n->elem << endl;
-				print_inorden(out, n->right);
+		BSNode<T>* remove(BSNode<T> *n, T e){
+			if(n == nullptr)
+				throw runtime_error("Value not found in tree\n");
+			else if(n->elem > e)
+				n->left = remove(n->left, e);
+			else if(n->elem < e)
+				n->right = remove(n->right, e);
+			else{
+				if(n->right != nullptr and n->left != nullptr){
+					n->elem = max(n->left);
+					n->left = remove_max(n->left);
+				}
+				else
+					n = (n->left != nullptr)? n->left: n->right;
 			}
+			
+			return n;
+		
+		}
+		
+		T max(BSNode<T>* n) const{
+			if(n == nullptr)
+				throw runtime_error("Tree empty\n");
+			else if(n->right != nullptr)
+				return max(n->right);
+			else
+				return n->elem;
+				
+		}
+		
+		BSNode<T>* remove_max(BSNode<T> *n){
+			if(n == nullptr)
+				throw runtime_error("Tree empty\n");
+			else if(n->right != nullptr)
+				n->right = remove_max(n->right);
+			else
+				return n->left;
+		
+		}
+		
+		void print_inorden(ostream &out, BSNode<T> *n) const{
+			if(n == nullptr) return;
+			
+			print_inorden(out, n->left);
+			out << n->elem << " ";
+			print_inorden(out, n->right);
+		}
+		
+		void delete_cascade(BSNode<T>* n){
+			if(n != nullptr){
+				delete_cascade(n->right);
+				delete_cascade(n->left);
+				delete n;
+			}
+		
 		}
 	
 	public:
-		BSTree();
+		BSTree(){
+		};
+		
+		~BSTree(){
+			BSNode<T> *aux = root;
+			delete_cascade(aux);
+		}
 		
 		int size()const {
 			return nelem;
 		}
 		
-		T search(T e) const {
-			return search(root, e);
+		void insert(T e){
+			BSNode<T> *aux = root;
+			aux = insert(aux, e);
+			nelem++;
 		}
 		
-		void insert(T e){
-			root = insert(root, e);
+		T search(T e) const {
+			BSNode<T> *aux = root;
+			return search(aux, e)->elem;
+		}
+		
+		T operator[](T e) const{
+			BSNode<T> *aux = root;
+			aux = search(aux, e);
+			T dato = aux->elem;
+			return dato;
 		}
 		
 		friend ostream& operator<<(ostream &out, const BSTree<T> &bst){
+			BSNode<T> *aux = bst.root;
+			bst.print_inorden(out, aux);
+			out << endl;
+			
+			return out;
+		}
 		
-			out << bst.print_inorden(out, bst->root);
-			return out;	
+		void remove(T e){
+			BSNode<T> *aux = root;
+			remove(aux, e);	
+			nelem--;
 		}
 };
 
